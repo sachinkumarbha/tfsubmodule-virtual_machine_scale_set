bfd4c9a7c0d80f6bacb6de874dd0ee01f8f1229b
wesley-trust <wesley-trust@users.noreply.github.com>
2022-09-02 16:15:30 +0100

    Initial code commit

---
+++ tests/integration_test.go
@@ -0,0 +1,102 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+func TestApplyWindows(t *testing.T) {
+	t.Parallel()
+
+	// Root folder where Terraform files should be (relative to the test folder)
+	rootFolder := "../"
+
+	// Path to Terraform example files being tested (relative to the root folder)
+	terraformFolderRelativeToRoot := "./examples/"
+
+	// Copy the terraform folder to a temp folder to prevent conflicts from parallel runs
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
+
+	// Generate a random deployment name for the test to prevent a naming conflict
+	uniqueID := random.UniqueId()
+	testREF := "Windows"
+	serviceDeployment := testREF + "-" + uniqueID
+	operatingSystemPlatform := "Windows"
+	resourceVmSku := "2022-datacenter-smalldisk-g2"
+
+	// Define variables
+	location := "UK South"
+
+	// Enable retryable error
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// The path to where the Terraform code is located
+		TerraformDir: tempTestFolder,
+
+		// Variables to pass to the Terraform code using -var options
+		Vars: map[string]interface{}{
+			"service_deployment":        serviceDeployment,
+			"resource_instance_count":   1,
+			"service_location":          location,
+			"operating_system_platform": operatingSystemPlatform,
+			"resource_vm_sku":           resourceVmSku,
+		},
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.DestroyE(t, terraformOptions)
+
+	// At the end of the test, run `terraform destroy` again, in case failures leave orphaned resources
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+}
+
+func TestApplyLinux(t *testing.T) {
+	t.Parallel()
+
+	// Root folder where Terraform files should be (relative to the test folder)
+	rootFolder := "../"
+
+	// Path to Terraform example files being tested (relative to the root folder)
+	terraformFolderRelativeToRoot := "./examples/"
+
+	// Copy the terraform folder to a temp folder to prevent conflicts from parallel runs
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
+
+	// Generate a random deployment name for the test to prevent a naming conflict
+	uniqueID := random.UniqueId()
+	testREF := "Linux"
+	serviceDeployment := testREF + "-" + uniqueID
+
+	// Define variables
+	location := "UK South"
+
+	// Enable retryable error
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// The path to where the Terraform code is located
+		TerraformDir: tempTestFolder,
+
+		// Variables to pass to the Terraform code using -var options
+		Vars: map[string]interface{}{
+			"service_deployment":      serviceDeployment,
+			"resource_instance_count": 1,
+			"service_location":        location,
+		},
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.DestroyE(t, terraformOptions)
+
+	// At the end of the test, run `terraform destroy` again, in case failures leave orphaned resources
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+}
---
+++ variables.tf
@@ -0,0 +1,80 @@
+# Required resource variables
+variable "name" {
+  description = "Desired name of the scale set"
+  type        = string
+}
+
+variable "source_image_id" {
+  description = "The image ID for the provisioned resources"
+  type        = string
+  default     = null
+}
+
+variable "location" {
+  description = "Desired location for the provisioned resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name for the provisioned resources"
+  type        = string
+}
+
+variable "operating_system_platform" {
+  description = "Desired OS for the provisioned resources"
+  type        = string
+}
+
+variable "size" {
+  description = "Desired size for the provisioned resources for each service"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Desired username for the provisioned resources"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Desired username for the provisioned resources"
+  type        = string
+  sensitive   = true
+}
+
+variable "subnet_id" {
+  description = "Subnet Id for scale set"
+  type        = string
+}
+
+variable "availability_set_id" {
+  description = "Availability set ID"
+  type        = string
+  nullable    = true
+}
+
+variable "zone" {
+  description = "Availability zones for the provisioned resources"
+  type        = string
+  nullable    = true
+}
+
+variable "disk_size_gb" {
+  description = "Desired disk size for the provisioned resources"
+  type        = string
+}
+
+variable "sku" {
+  description = "Desired sku for the provisioned resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Desired environment for the service collection of provisioned resources"
+  type        = string
+}
+
+variable "instances" {
+  description = "Number of instances of the provisioned resources"
+  type        = string
+  default     = 1
+}
